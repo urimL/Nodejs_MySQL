472efc72d8b416d4fa62e7277a164cfec78a6feb
toulim <77326325+urimL@users.noreply.github.com>
2022-09-22 21:47:30 +0900

    상세보기 구현

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var mysql = require('mysql');
 
+//mysql에 접속
 var db = mysql.createConnection({
   host: 'localhost',
   user: 'nodejs',
-  password: '1111',
+  password: 'dbfla9771!',
   database : 'opentutorial'
 });
 db.connect();
@@ -21,10 +22,10 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        /*fs.readdir('./data', function(error, filelist){
+        /*fs.readdir('./data', function(error, topics){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.HTML(title, list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>`
@@ -32,13 +33,28 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(html);
         });*/
+        
+        //mysql의 데이터를 main.js로 load해오는 방법
         db.query(`SELECT * FROM topic`,function(error,topics) {
           console.log(topics);
+          var title = 'Welcome';
+          var description = 'Hello, NodeJs';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
           response.writeHead(200);
-          response.end('success');
+          response.end(html);
         });
-      } else {
-        fs.readdir('./data', function(error, filelist){
+        //배열의 객체로 여러가지 propery 값이 세팅되어 출력
+
+      } 
+      
+      //상세보기
+      else {
+        /*
+        fs.readdir('./data', function(error, topics){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
@@ -46,7 +62,7 @@ var app = http.createServer(function(request,response){
             var sanitizedDescription = sanitizeHtml(description, {
               allowedTags:['h1']
             });
-            var list = template.list(filelist);
+            var list = template.list(topics);
             var html = template.HTML(sanitizedTitle, list,
               `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
               ` <a href="/create">create</a>
@@ -60,11 +76,39 @@ var app = http.createServer(function(request,response){
             response.end(html);
           });
         });
+        */
+
+
+        //글 목록 가져오기
+        db.query(`SELECT * FROM topic`, function(error,topics){
+          if(error){
+            throw error;
+          }
+          db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+            if(error2){
+              throw error2;
+            }
+           var title = topic[0].title;
+           var description = topic[0].description;
+           var list = template.list(topics);
+           var html = template.HTML(title, list,
+             `<h2>${title}</h2>${description}`,
+             ` <a href="/create">create</a>
+                 <a href="/update?id=${queryData.id}">update</a>
+                 <form action="delete_process" method="post">
+                   <input type="hidden" name="id" value="${queryData.id}">
+                   <input type="submit" value="delete">
+                 </form>`
+           );
+            response.writeHead(200);
+            response.end(html);
+          })
+        });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
+      fs.readdir('./data', function(error, topics){
         var title = 'WEB - create';
-        var list = template.list(filelist);
+        var list = template.list(topics);
         var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host : 'localhost',
     user : 'nodejs',
-    password : '1111',
+    password : 'dbfla9771!',
     database : 'opentutorial'
 });
 
