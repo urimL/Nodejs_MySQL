c484b70007bff342ee3baed4736e6b32a9640004
toulim <toulim0525@ewhain.net>
2022-09-26 14:08:30 +0900

    author_update, delete

--- main.js
+++ main.js
@@ -26,17 +26,23 @@ var app = http.createServer(function (request, response) {
     } else if (pathname === '/create') {
         topic.create(request, response);
     } else if (pathname === '/create_process') {
-        topic.create_proccess(request, response);
+        topic.create_process(request, response);
     } else if (pathname === '/update') {
         topic.update(request, response);
     } else if (pathname === '/update_process') {
-        topic.update_proccess(request, response);
+        topic.update_process(request, response);
     } else if (pathname === '/delete_process') {
-        topic.delete_proccess(request, response);
+        topic.delete_process(request, response);
     } else if (pathname === '/author') {
         author.home(request, response);
-    } else if(pathname ==='/author/create_proccess'){
-        author.create_proccess(request,response);
+    } else if(pathname ==='/author/create_process'){
+        author.create_process(request,response);
+    } else if(pathname ==='/author/update'){
+        author.update(request,response);
+    } else if(pathname ==='/author/update_process'){
+        author.update_process(request,response);
+    } else if(pathname ==='/author/delete_process'){
+        author.delete_process(request,response);
     } else {
         response.writeHead(404);
         response.end('Not found');
